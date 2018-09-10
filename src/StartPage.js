686f4477354d727d03d4c9369af5ae79837bb861
Dmitry Kharitonov <d.kharitonov@gmail.com>
2018-09-10 23:14:01 +0300

    filter items and dislpay snippets by type

--- src/StartPage.js
+++ src/StartPage.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './StartPage.css'
 
 export default class StartPage extends Component {
+
+  contentTypes = [
+    'article',
+    'disgest',
+    'report'
+  ];
+
   constructor(props) {
     super(props);
 
@@ -14,6 +21,12 @@ export default class StartPage extends Component {
 
     this._isMounted = false;
     this.content = this.props.content;
+
+    [
+      'setItems',
+      'reportError',
+      'matchContentByType'
+    ].map(f => this[f] = this[f].bind(this));
   }
 
   componentDidMount() {
@@ -22,7 +35,7 @@ export default class StartPage extends Component {
     this.setState({
       isLoading: true
     });
-    this.content.requestItems(this.setItems.bind(this), this.reportError.bind(this))
+    this.content.requestItems(this.setItems, this.reportError)
   }
 
   componentWillUnmount() {
@@ -32,7 +45,7 @@ export default class StartPage extends Component {
   setItems(items) {
     if(this._isMounted) {
       this.setState({
-        items,
+        items: this.filterItems(items.items),
         isLoading: false,
         isLoaded: true
       });
@@ -53,11 +66,91 @@ export default class StartPage extends Component {
     console.log('error with getting content', error);
   }
 
+  getContentTypes = () => this.contentTypes;
+
+  matchContentByType = (item) =>
+    this.getContentTypes()
+      .reduce((acc, value) => acc || value === item.sys.contentType.sys.id,false);
+
+  filterItems = (items) => items.filter(item => this.matchContentByType(item));
+
   render() {
     return (
       <div className="main-page">
-        main page
+        { this.state.isLoading
+          ? <div>Loading</div>
+          : this.state.items.length === 0
+            ? <div>no items</div>
+            : this.renderItems()
+        }
       </div>
     );
   }
+
+  renderItems() {
+    return (
+      <div className="items">
+        {
+          this.state.items.map( item => {
+
+            const id = item.sys.id;
+            const {
+              title,
+              published,
+              publisher,
+              cover,
+              spoiler
+            } = item.fields;
+
+            switch(item.sys.contentType.sys.id) {
+              case 'article':
+                return this.renderArticleSnippet({id, title, published});
+              case 'report':
+                return this.renderReportSnippet({id, title, published, publisher, cover});
+              case 'disgest':
+                return this.renderDigestSnippet({id, title, published, publisher, cover, spoiler});
+              default:
+                return (
+                  <div>неправильный тип данных {item.sys.contentType.sys.id} </div>
+                )
+            }
+          })
+        }
+      </div>
+    )
+  }
+
+  renderArticleSnippet({id, title, published}) {
+    return (
+      <div className="article-snippet" key={id}>
+        <div className="article-snippet--title">{title}</div>
+        <div>{published} | {id}</div>
+      </div>
+    )
+  }
+
+  renderReportSnippet({id, title, published, publisher, cover}) {
+    console.log(cover.fields.file);
+    return (
+      <div className="report-snippet" key={id}>
+        <div className="report-snippet--title">{title}</div>
+        <div className="report-snippet--publisher">{publisher}</div>
+        <div>{published}</div>
+        <div className="report-snippet--cover"><img src={cover.fields.file.url} alt={cover.fields.title}/></div>
+      </div>
+    )
+  }
+
+  renderDigestSnippet({id, title, published, publisher, cover, spoiler}) {
+    return (
+      <div className="digest-snippet" key={id}>
+        <div className="digest-snippet--title">{title}</div>
+        <div className="digest-snippet--publisher">{publisher}</div>
+        <div className="digest-snippet--spoiler">{spoiler}</div>
+        <div>{published}</div>
+        <div className="digest-snippet--cover"><img src={cover.fields.file.url} alt={cover.fields.title}/></div>
+      </div>
+    )
+  }
+
 }
